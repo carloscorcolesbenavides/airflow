8e09bff33a7f85cd639603db791aad1f3d3f5e2b
carloscorcolesbenavides <carlos.corcoles.benavides@gmail.com>
2026-06-01 19:42:23 +0200

    update dag_de_prueba.py

--- airflow_udemy/dags/dag_de_prueba.py
+++ airflow_udemy/dags/dag_de_prueba.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from airflow.models import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 
@@ -19,7 +19,7 @@ def hello_world_loop():
 with DAG('dag_prueba',
          default_args=default_args,
          schedule_interval='@daily') as dag:
-    start = DummyOperator(task_id='start')
+    start = EmptyOperator(task_id='start')
 
     prueba_python = PythonOperator(task_id='prueba_python',
                                    python_callable=hello_world_loop)
