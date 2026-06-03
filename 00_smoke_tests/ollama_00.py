4bb455f2efcfd9fcd03f037dc53fbf01db435c92
carloscorcolesbenavides <carlos.corcoles.benavides@gmail.com>
2026-06-03 18:44:09 +0200

    update 'ollama_00.py

--- 00_smoke_tests/ollama_00.py
+++ 00_smoke_tests/ollama_00.py
@@ -1,5 +1,8 @@
 from langchain_ollama import ChatOllama
 
-llm = ChatOllama(model="phi3:mini")
+llm = ChatOllama(
+    model="phi3:mini",
+    base_url="http://localhost:36813"
+)
 response = llm.invoke("Hola, ¿qué puedes hacer?")
 print(response.content)
